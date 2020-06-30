237f5af8d33f4f3364ead14487d8bfed82b6502a
Slugin Roman <sluginroma@yandex.ru>
2020-06-30 18:20:05 +0300

    Event date and time. Update CSS

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.fields.html5 import DateField, TimeField
 from wtforms.validators import DataRequired, Email
 
 class EventForm(FlaskForm):
-    name = StringField("Name ", validators=[DataRequired()])
-    start_time = StringField("Event start time ", validators=[DataRequired()])
-    end_time = StringField("Event end time ", validators=[DataRequired()])
-    description = StringField("Description ")
+    name = StringField("Event name", validators=[DataRequired()])
+    start_time = TimeField("Event start time", validators=[DataRequired()])
+    end_time = TimeField("Event end time", validators=[DataRequired()])
+    start_date = DateField("Event start date", validators=[DataRequired()])
+    end_date = DateField("Event end date", validators=[DataRequired()])
+    description = TextAreaField("Description ")
     submit = SubmitField("Submit")
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app import app, db
 from .forms import EventForm
 from app.models import Event
 
-from datetime import date
+import datetime
 
 
 @app.route('/')
@@ -19,22 +19,22 @@ def make_event():
         event_name = form.name.data
         event_start_time = form.start_time.data
         event_end_time = form.end_time.data
+        event_start_date = form.start_date.data
+        event_end_date = form.end_date.data
         event_description = form.description.data
         
-        start_time = event_start_time.split('.')
-        start_year = int(start_time[0])
-        start_month = int(start_time[1])
-        start_day = int(start_time[2])
-        
-        end_time = event_end_time.split('.')
-        end_year = int(end_time[0])
-        end_month = int(end_time[1])
-        end_day = int(end_time[2])
-        
-        u = Event(name=event_name, start_time=date(start_year, start_month, start_day), end_time=date(end_year, end_month, end_day), description=event_description)
+        event_start_full_time = datetime.datetime.combine(event_start_date, event_start_time)
+        event_end_full_time = datetime.datetime.combine(event_end_date, event_end_time)
+
+        u = Event(name=event_name, start_time=event_start_full_time, end_time=event_end_full_time, description=event_description)
         db.session.add(u)
         db.session.commit()
-        #print(event_description, event_end_time, event_name, event_start_time)
+        # print(event_name, ' ', type(event_name), '\n', \
+            #   event_start_time, type(event_start_time), '\n', \
+            #   event_start_date, type(event_start_date), '\n', \
+            #   event_end_time, type(event_end_time), '\n', \
+            #   event_end_date, type(event_end_date), '\n', \
+            #   event_description,)
         
     return render_template('event.html', form=form)
 
